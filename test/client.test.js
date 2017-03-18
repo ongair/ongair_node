bbac1c75bce867cc9302ef8931b610c239e4225c
Trevor Kimenye <kimenye@gmail.com>
2017-03-18 20:19:59 +0300

    Added thread function

---
+++ test/client.test.js
@@ -0,0 +1,47 @@
+const chai = require('chai')
+const sinon = require('sinon')
+const request = require('request')
+const { expect } = chai
+
+const Client = require('../lib/client')
+
+
+describe('The ongair client', () => {
+
+  describe('Sending a message', () => {
+    before(() => {
+      sinon
+        .stub(request, 'post')
+        .yields(null, { statusCode: 200 }, JSON.stringify({ success: true }));
+    })
+
+    it('Can send a text message', (done) => {
+
+      let client = new Client('token', 'https://staging.ongair.im')
+
+      client.sendMessage('12345', 'Hi', "Yes,No")
+        .then(success => {
+          expect(success).to.be.true
+          done()
+        })
+        .catch(ex => {
+          done()
+        })
+
+    })
+  })
+
+  describe('Sending a thread', () => {
+
+    it('Can send a chain with options', (done) => {
+      let client = new Client('token', 'https://staging.ongair.im')
+      client.sendThread('12345', [{text: 'Hi', options: ['Yes', 'No']}])
+        .then(success => {
+          expect(success).to.be.true
+          done()
+        })
+    })
+
+  })
+
+})
