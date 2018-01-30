ca672b3ed1fffe09b6050bc3ca7ea2bcc810107b
Trevor Kimenye <kimenye@gmail.com>
2018-01-30 12:56:56 +0300

    Added tagging of replies

--- test/client.test.js
+++ test/client.test.js
@@ -1,6 +1,7 @@
 const chai = require('chai')
-const sinon = require('sinon')
-const request = require('request')
+// const sinon = require('sinon')
+// const request = require('request')
+const nock = require('nock')
 const { expect } = chai
 
 const Client = require('../lib/client')
@@ -9,15 +10,14 @@ const Client = require('../lib/client')
 describe('The ongair client', () => {
 
   describe('Sending a message', () => {
-    before(() => {
-      sinon
-        .stub(request, 'post')
-        .yields(null, { statusCode: 200 }, { success: true });
-    })
 
     it('Can send a text message', (done) => {
 
       let client = new Client('token', 'https://staging.ongair.im')
+      nock('https://staging.ongair.im')
+        .post('/send', "token=token&external_id=12345&thread=true&text=Hi&reply_options=Yes%2CNo&tags=")
+        .reply(200, JSON.stringify({ success: true }))
+
 
       client.sendMessage('12345', 'Hi', "Yes,No")
         .then(success => {
@@ -35,6 +35,11 @@ describe('The ongair client', () => {
 
     it('Can send a chain with options', (done) => {
       let client = new Client('token', 'https://staging.ongair.im')
+
+      nock('https://staging.ongair.im')
+      .post('/send_chain', {"external_id":"12345","thread":true,"tags":"","messages":[{"text":"Hi","options":["Yes","No"]}]})
+      .reply(200, JSON.stringify({ success: true }))
+
       client.sendThread('12345', [{text: 'Hi', options: ['Yes', 'No']}])
         .then(success => {
           expect(success).to.be.true
